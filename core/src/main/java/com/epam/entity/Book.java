127dfb3ffe26d6e8b0c2e64b380e0e3d7c075d22
InfinityQX70s <mazumisha@gmail.com>
2016-02-18 21:07:24 +0300

    Add Entity

---
+++ core/src/main/java/com/epam/entity/Book.java
@@ -0,0 +1,55 @@
+package com.epam.entity;
+
+import java.util.Date;
+
+/**
+ * Created by infinity on 18.02.16.
+ */
+public class Book {
+
+    private int id;
+    private String name;
+    private Date year;
+    private int authorId;
+    private int genreId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getYear() {
+        return year;
+    }
+
+    public void setYear(Date year) {
+        this.year = year;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public int getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(int genreId) {
+        this.genreId = genreId;
+    }
+}
---
+++ core/src/main/java/com/epam/entity/BookOrder.java
@@ -0,0 +1,44 @@
+package com.epam.entity;
+
+/**
+ * Created by infinity on 18.02.16.
+ */
+public class BookOrder {
+
+    private int id;
+    private int userId;
+    private int bookId;
+    private int statusId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public int getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(int statusId) {
+        this.statusId = statusId;
+    }
+}
